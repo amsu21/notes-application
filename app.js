8ce3e68476b7b636572750d55dbed75c90cc34a1
Amsu Warner <aswb4@hotmail.com>
2021-08-16 22:54:56 -0400

    Added access to form

--- app.js
+++ app.js
@@ -1,13 +1,2 @@
-// ACCES FORM
-let form = querySelector(".form");
-
-// GET DATA
-document.querySelectorAll("#myForm input");
-
-
-// EVENT LISTENERS
-
-
-// FUNCTION
-Array.from(document.querySelectorAll("#myForm input")).reduce((acc, input) => ({
-    ...acc, [input.id]: input.value }), {});
+// ACCESS FORM
+let form = querySelector("#myForm");
