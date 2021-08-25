4a75922226950e5aea7c43b2b5b150e970fed760
Amsu Warner <aswb4@hotmail.com>
2021-08-25 12:01:35 -0400

    Added button event listener

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 // ACCESS FORM AND BUTTON
 const form = document.getElementById('myForm');
 const button = document.getElementById('submitButton');
+
+// EVENT LISTENER FOR BUTTON
+document.getElementById('submitButton').addEventListener('click', () => {
+    alert("CLicked!!");
+});
