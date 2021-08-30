8b318b89ccb51bb1defe4ca8aa558d2ed6f49545
Amsu Warner <aswb4@hotmail.com>
2021-08-30 17:35:59 -0400

    Added functions

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const form = document.getElementById('myForm');
 const button = document.getElementById('submitButton');
 
 // EVENT LISTENER FOR BUTTON
-document.getElementById('submitButton').addEventListener('click', () => {
-    alert("CLicked!!");
-});
+ document.getElementById('submitButton').addEventListener('click', () => {
+     receiveNotes();
+ });
+
+// DATA FUNCTION
+function receiveNotes() {
+    var note = document.getElementById('input_id').value;
+    document.getElementById('alert').innerHTML = "The user input is: " + note;
+}
